de7865226e704adc1a0e2263970467efb8911844
Elijah <elieel@gmail.com>
2016-10-05 22:23:57 -0400

    Complete Queries exercises for three table .sql file.

--- exercises2.sql
+++ exercises2.sql
@@ -126,9 +126,27 @@ values
 );
 
 -- Insert info into table review
-INSERT INTO review (author_reviewer_id, stars, title, review, restaurant_id) values (4, 5, 'Best Indian Food Ever', 'The only time I had better Indian food was at Trump Tower!', 1);
-INSERT INTO review (author_reviewer_id, stars, title, review, restaurant_id) values (1, 1, 'Sub-PAR-way more like!', 'Went in for five footlongs and they forgot to put double mayo on one! Last time I ever eat there #talkaboutshittyservice', 2);
-INSERT INTO review (author_reviewer_id, stars, title, review, restaurant_id) values (1, 4, 'The only fine dining Mrs. Winner''s found outside of North Korea!', 'I had my doubts, but I am now thouroughly convinced that all Mrs. Winner's should be retro converted to emulate this establishments fine dining atmosphere. The food was delicious (of course), but you can't beat that high class feeling you get when you walk across the cracked parking lot on the side of I-20!', 8);
+INSERT INTO review
+(
+  reviewer_id,
+  stars,
+  title,
+  review,
+  restaurant_id
+)
+values
+(
+  4,
+  5,
+  'Best Indian Food Ever',
+  'The only time I had better Indian food was at Trump Tower!',
+  1
+),
+(1, 1, 'Sub-PAR-way more like!', 'Went in for five footlongs and they forgot to put double mayo on one! Last time I ever eat there #talkaboutshittyservice', 2)
+(1, 4, 'The only fine dining Mrs. Winner''s found outside of North Korea!', 'I had my doubts, but I am now thouroughly convinced that all Mrs. Winner's should be retro converted to emulate this establishments fine dining atmosphere. The food was delicious (of course), but you can't beat that high class feeling you get when you walk across the cracked parking lot on the side of I-20!', 8)
+;
+INSERT INTO review (author_reviewer_id, stars, title, review, restaurant_id) values ;
+INSERT INTO review (author_reviewer_id, stars, title, review, restaurant_id) values ;
 INSERT INTO review (author_reviewer_id, stars, title, review, restaurant_id) values (2, 3, 'In a word, "meh".', ' took my girlfriend Heather there, and though the food quality wasn''t bad, we were thouroughly unimpressed with the clientelle. Wound up murdering all of them, but made it look like a mass suicide! HA!', 4);
 INSERT INTO review (author_reviewer_id, stars, title, review, restaurant_id) values (3, 1.22234, 'Papa don''t dance with no squares!', 'I enjoy cats the most because the Russians didn''t send one to space?', 11);
 
@@ -146,11 +164,12 @@ select name, count(review) from restaurant, review where restaurant.id = review.
 -- 6. list each review along with the restaurant, and the reviewer's name. Select the restaurant name, the review text, and the reviewer name
 select review, reviewer.name, restaurant.name from review, restaurant, reviewer where review.reviewer_id = reviewer.id and restaurant.id = review.reviewer_id group by restaurant.name;
 -- 7. get the average stars by reviewer (reviewer name, average star rating)
-
+select name, round(avg(stars), 2) from reviewer, review where reviewer.id = review.reviewer_id group by reviewer.id;
 -- 8. get the lowest star rating for each reviewer (reviewer name, lowest star rating)
-
+select name, min(stars) from reviewer, review where reviewer.id = review.reviewer_id group by reviewer.id;
 -- 9. get the number of restaurants in each category (category name, restaurant count)
-
+select category, count(category) from restaurant group by category;
 -- 10. get number of 5 star reviews by restaurant (restaurant name, 5-star count)
-
+select restaurant.name, review.stars from restaurant, review where stars = 5 and restaurant.id = review.restaurant_id group by restaurant.name, review.stars;
 -- 11. average star rating for a food category (category name, average star rating)
+select category, round(avg(stars), 2) from restaurant, review where restaurant.id = review.restaurant_id group by restaurant.category;
