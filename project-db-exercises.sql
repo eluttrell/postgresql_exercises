ac2f9c0c7585814df6ed545ad16e052862ca2440
Elijah <elieel@gmail.com>
2016-10-05 22:48:23 -0400

    Finish exercise three from project_db exercises: Perform a left outer join between ech and project_uses_tech table.

--- project-db-exercises.sql
+++ project-db-exercises.sql
@@ -3,7 +3,7 @@ select * from project, tech;
 -- 2. Perform a left outer join between the project table and the project_uses_tech tables
 select * from project left outer join project_uses_tech on project_uses_tech.project_id = project.id;
 -- 3. Perform a left outer join between the tech table and the project_uses_tech table
-
+select * from tech left outer join project_uses_tech on project_uses_tech.tech_id = tech.id;
 -- 4. Perform a left outer join from the project table to the project_users_tech table and then left outer join again to the tech table.
 
 -- 5. Start from the answer on the previous problem and get the count of how many different tech each project uses
